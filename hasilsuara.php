b3355e91a2725dca9d4b58fb5ed3eda598824a40
Lab RPL 1 <151610205@smkassalaambandung.sch.id>
2017-10-24 13:54:50 +0400

    latihan2

---
+++ hasilsuara.php
@@ -0,0 +1,29 @@
+<?php 
+require_once'suara.php';
+
+$identitas=new suara('Mboooo','Mbeeee','Meonggg','Mhooooo');
+echo "Kambing..".$identitas->kambing.'<br>';
+echo "Domba..".$identitas->domba.'<br>';
+echo "Kucing..".$identitas->kucing.'<br>';
+echo "Sapi..".$identitas->sapi.'<br>'.'<br>';
+
+$identitas=new suara('Mboooo','Mbeeee','Meonggg','Mhooooo');
+echo "Kambing..".$identitas->kambing.'<br>';
+echo "Domba..".$identitas->domba.'<br>';
+echo "Kucing..".$identitas->kucing.'<br>';
+echo "Sapi..".$identitas->sapi.'<br>'.'<br>';
+
+$identitas=new suara('Mboooo','Mbeeee','Meonggg','Mhooooo');
+echo "Kambing..".$identitas->kambing.'<br>';
+echo "Domba..".$identitas->domba.'<br>';
+echo "Kucing..".$identitas->kucing.'<br>';
+echo "Sapi..".$identitas->sapi.'<br>'.'<br>';
+
+$identitas=new suara('Mboooo','Mbeeee','Meonggg','Mhooooo');
+echo "Kambing..".$identitas->kambing.'<br>';
+echo "Domba..".$identitas->domba.'<br>';
+echo "Kucing..".$identitas->kucing.'<br>';
+echo "Sapi..".$identitas->sapi.'<br>'.'<br>';
+
+
+ ?>
---
+++ suara.php
@@ -0,0 +1,15 @@
+<?php
+class suara{
+	public $kambing;
+   	public $domba ;
+   	public $kucing;
+   	public $sapi;
+   	   public function __construct($ka,$do,$ku,$sa)
+   	   {
+     $this->kambing=$ka;
+     $this->domba=$do;
+     $this->kucing=$ku;
+     $this->sapi=$sa;
+}
+}
+  ?>
